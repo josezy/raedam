48d72bae5c8cc0ceee306da64ee726cfe1190ae3
Jose B <jose.zdy@gmail.com>
2019-09-01 14:55:00 -0500

    show spots in map

--- server.py
+++ server.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python3
 
 # import sys
-# import json
+import json
 # from datetime import datetime
 
-from flask import Flask, render_template#, redirect
+from flask import Flask, render_template, jsonify
 
 
-### Config
+# Config
 app = Flask(__name__)
-# CONFIG_FILE = 'content.json'
-# HOST = 'http://127.0.0.1:5000'
-
-# def load_config(fname=CONFIG_FILE):
-#     """read the content.json file and load it as a dictionary"""
-#     with open(fname, 'r') as f:
-#         return json.load(f)
-
-# CONFIG = load_config(CONFIG_FILE)
-
-# PAGES = {page['url']: page for page in  list(CONFIG['PAGES'].values())}  # {url: {page_data}}
-# POSTS = {post['url']: post for post in  list(CONFIG['POSTS'].values())}  # {url: {post_data}}
+CONFIG_FILE = 'config.json'
 
 
-### Routes
+def load_config(fname=CONFIG_FILE):
+    """read the config.json file and load it as a dictionary"""
+    with open(fname, 'r') as f:
+        return json.load(f)
 
-@app.route('/')
+
+CONFIG = load_config(CONFIG_FILE)
+
+
+# Routes
+@app.route('/', methods=['GET'])
 def index():
-    # return redirect("/index.html")
-    return render_template('index.html')
+    return render_template('index.html', **CONFIG)
+
+
+@app.route('/spots', methods=['GET'])
+def available_spots():
+    spots = [
+        {
+            'name': "Spot 1",
+            'free': True,
+            'coords': [-75.378102, 6.149930],
+        },
+        {
+            'name': "Spot 2",
+            'free': False,
+            'coords': [-75.378106, 6.149850]
+        }
+    ]
+    return jsonify(spots)
 
 # @app.route('/favicon.ico')
 # def favicon():
@@ -37,7 +50,12 @@ def index():
 # @app.route('/<path>')
 # def render_page(path):
 #     page = PAGES[f'/{path}']
-#     return render_template(page['template'], now=datetime.now(), **CONFIG, **page)
+#     return render_template(
+#         page['template'],
+#         now=datetime.now(),
+#         **CONFIG,
+#         **page
+#     )
 
 # @app.route('/posts/<path>')
 # def render_post(path):
