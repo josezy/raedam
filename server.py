484778d3156749120eaee1a9d257049d986f1973
Jose B <jose.zdy@gmail.com>
2019-09-10 01:16:13 -0500

    spot endpoints (create, delete, update, get state)

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import json
+import random
 
-from flask import Flask, render_template, jsonify, redirect
+from flask import Flask, render_template, jsonify, redirect, abort
+from util import load_json_file, save_json_file, parse_coords
 
 
 # Config
 app = Flask(__name__)
 CONFIG_FILE = 'config.json'
+SPOTS_FILE = 'spots.json'
 
 
 def load_config(fname=CONFIG_FILE):
@@ -20,6 +23,11 @@ CONFIG = load_config(CONFIG_FILE)
 
 
 # Routes
+@app.route('/favicon.ico')
+def favicon():
+    return redirect("/static/favicon.ico")
+
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', **CONFIG)
@@ -27,24 +35,77 @@ def index():
 
 @app.route('/spots', methods=['GET'])
 def available_spots():
-    spots = [
-        {
-            'id': 1234,
-            'free': True,
-            'coords': [-75.378102, 6.149930],
-        },
-        {
-            'id': 1235,
-            'free': False,
-            'coords': [-75.378106, 6.149850]
-        }
-    ]
+    spots = load_json_file(SPOTS_FILE)
     return jsonify(spots)
 
 
-@app.route('/favicon.ico')
-def favicon():
-    return redirect("/static/favicon.ico")
+@app.route('/spot/register/<coords>', methods=['POST'])
+def register_spot(coords):
+    try:
+        lon, lat = parse_coords(coords)
+    except ValueError:
+        return jsonify({
+            'success': False,
+            'message': 'Could not parse coordinates'
+        })
+
+    spots = load_json_file(SPOTS_FILE)
+    spot_ids = [spot['id'] for spot in spots]
+
+    try:
+        new_id = random.choice(list(set(range(1000, 10000)) - set(spot_ids)))
+    except IndexError:
+        return jsonify({
+            'success': False,
+            'message': 'Could not assign an ID'
+        })
+
+    spots = spots + [{
+        'id': new_id,
+        'free': True,
+        'coords': [lon, lat]
+    }]
+
+    save_json_file(SPOTS_FILE, spots)
+    return jsonify({'success': True})
+
+
+@app.route('/spot/delete/<int:spot_id>', methods=['POST'])
+def delete_spot(spot_id):
+    spots = load_json_file(SPOTS_FILE)
+    updated_spots = [spot for spot in spots if spot['id'] != spot_id]
+    save_json_file(SPOTS_FILE, updated_spots)
+    return jsonify({'success': True})
+
+
+@app.route('/spot/<int:spot_id>/<state>', methods=['POST'])
+def update_spot(spot_id, state):
+    if state not in ['free', 'busy']:
+        abort(404)
+
+    spots = load_json_file(SPOTS_FILE)
+    matches = [spot for spot in spots if spot['id'] == spot_id]
+    if len(matches) != 1:
+        abort(404)
+
+    matches[0]['free'] = True if state == 'free' else False
+    updated_spot = matches[0]
+    updated_spots = [
+        updated_spot if spot['id'] == spot_id else spot
+        for spot in spots
+    ]
+    save_json_file(SPOTS_FILE, updated_spots)
+
+    return jsonify({'success': True})
+
+
+@app.route('/spot/<int:spot_id>', methods=['POST'])
+def spot_state(spot_id):
+    spots = load_json_file(SPOTS_FILE)
+    matches = [spot for spot in spots if spot['id'] == spot_id]
+    if len(matches) != 1:
+        abort(404)
+    return jsonify(matches[0])
 
 
 if __name__ == '__main__':
---
+++ util.py
@@ -0,0 +1,41 @@
+import os
+import json
+
+
+def append_json_file(filepath, new_data):
+    data_collected = load_json_file(filepath)
+
+    # append data to 'data_collected' dict
+    data_collected[len(data_collected)] = new_data
+
+    save_json_file(filepath, data_collected)
+
+
+def save_json_file(filepath, json_data):
+    with open(filepath, 'w') as fp:
+        json.dump(json_data, fp)
+
+
+def load_json_file(filepath):
+    if not os.path.isfile(filepath):
+        with open(filepath, 'w+') as fp:
+            json.dump({}, fp)
+        return {}
+
+    with open(filepath, 'r+') as fp:
+        try:
+            data = json.load(fp)
+        except json.JSONDecodeError:
+            fp.truncate(0)
+            json.dump({}, fp)
+            data = {}
+    return data
+
+
+def parse_coords(coords):
+    _coords = "".join(coords.split()).split(',')
+    if len(_coords) != 2:
+        raise ValueError(
+            f"Coordinates format is 'xx.xxxx,yy.yyyy'. Got '{coords}' instead"
+        )
+    return float(_coords[0]), float(_coords[1])
