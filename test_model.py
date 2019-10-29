b2381b7de077e80c3bea89f932e848f9ed35be40
Jose B <jose.zdy@gmail.com>
2019-10-29 14:48:26 -0500

    set devorg schema context on createdemodata and testmodel

--- test_model.py
+++ test_model.py
@@ -9,27 +9,29 @@ sys.path.append("./raedamdjango/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django.setup()
 
+from tenant_schemas.utils import schema_context
 from parking.models import ParkingCamera
 
 
-CAM_SHORT_ID = 'b70e6965'
+CAM_SHORT_ID = '787a8c5f'
 
-cam = ParkingCamera.objects.get(id__startswith=CAM_SHORT_ID)
-cap = cv2.VideoCapture(cam.url)
-success, frame = cap.read()
-if not success:
-    raise ValueError(f"[!] Cannot read from source '{cam.url}'")
+with schema_context('devorg'):
+    cam = ParkingCamera.objects.get(id__startswith=CAM_SHORT_ID)
+    cap = cv2.VideoCapture(cam.url)
+    success, frame = cap.read()
+    if not success:
+        raise ValueError(f"[!] Cannot read from source '{cam.url}'")
 
-rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-model = load_model()
-r = model.detect([rgb_frame], verbose=0)[0]
-cleaned_boxes = clean_boxes(r, ['car', 'bus', 'truck'], 0.8)
-cam.spots = cleaned_boxes.tolist()
-cam.save()
+    model = load_model()
+    r = model.detect([rgb_frame], verbose=0)[0]
+    cleaned_boxes = clean_boxes(r, ['car', 'bus', 'truck'], 0.8)
+    cam.spots = cleaned_boxes.tolist()
+    cam.save()
 
-for box in cleaned_boxes:
-    y1, x1, y2, x2 = box
-    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    for box in cleaned_boxes:
+        y1, x1, y2, x2 = box
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
 
-cv2.imwrite('data/pics/test.png', frame)
+    cv2.imwrite('data/pics/test.png', frame)
