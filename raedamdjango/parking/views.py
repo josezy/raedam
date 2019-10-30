da4ee5eaee49ec657a500018eb364d06554232ff
Jose B <jose.zdy@gmail.com>
2019-10-29 21:10:02 -0500

    revert commented out code. clean

--- raedamdjango/parking/views.py
+++ raedamdjango/parking/views.py
@@ -1,100 +1,71 @@
 import cv2
-# import mrcnn
+import json
+import time
+import mrcnn
 import numpy as np
-# import tensorflow as tf
+import tensorflow as tf
 
 from django.db import connection
 from django.conf import settings
 from django.views import View
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView,CreateView
+from django.views.generic import ListView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from typing import Dict, Any
 from decimal import Decimal
 
-import json
-import base64
-
 from core.utils import coords_in_radius
-from core.model_utils import Camera
 from parking.models import ParkingCamera
-'''from cerebro import load_model, clean_boxes
+from cerebro import load_model, clean_boxes
 
 
 if settings.ENABLE_DETECTOR and settings.IS_RUNSERVER:
     model = load_model()
     graph = tf.get_default_graph()
 
-'''
 
-
-
-
-class CamDeleteView(DeleteView): 
-	model = ParkingCamera
-	template_name = 'parking/cam_delete.html'
-	success_url = reverse_lazy('parking/cam_list.html')
-
-class CamListView(ListView):
-	model = ParkingCamera
-	template_name = 'parking/cam_list.html'
-    
-def manage_spots(request,cam_id):
+def manage_spots(request, cam_id):
     return render(request, 'parking/manage_spots.html', {'probe': "probe"})
 
 
-class ManageSpots(UpdateView):
-    model = ParkingCamera
-    fields = ('spots', 'geopoint',)
-    template_name = 'parking/manage_spots.html'
-
-
 def remove_zone(request):
-    SPOT_DATA=request.GET.get('SPOT_DATA')
-    SPOT_DATA=json.loads(SPOT_DATA)
+    SPOT_DATA = json.loads(request.GET.get('SPOT_DATA'))
     camera_of_spots = ParkingCamera.objects.get(pk=SPOT_DATA['id_cam'])
-    camera_of_spots.spots=SPOT_DATA['spots']
+    camera_of_spots.spots = SPOT_DATA['spots']
     camera_of_spots.save()
-    return JsonResponse(SPOT_DATA,safe=False)
-       
+    return JsonResponse(SPOT_DATA, safe=False)
+
 
 def add_new_zone(request):
-
-    SPOT_DATA=json.loads(request.GET.get('SPOT_DATA'))
-    print("\n")
-    print(SPOT_DATA)
-    print("\n")
-    id_cam=SPOT_DATA.id_cam
+    SPOT_DATA = json.loads(request.GET.get('SPOT_DATA'))
+    id_cam = SPOT_DATA.id_cam
     x = SPOT_DATA.x
     y = SPOT_DATA.y
     width = SPOT_DATA.width
     height = SPOT_DATA.height
-    variable1= {
-            'id_cam': id_cam,
-            'x':x,
-            'y':y,
-            'width':width,
-            'height':height,
-            'spots':[],
-        }
-    
+    zone_data = {
+        'id_cam': id_cam,
+        'x': x,
+        'y': y,
+        'width': width,
+        'height': height,
+        'spots': [],
+    }
+
     try:
         camera_of_spots = ParkingCamera.objects.get(pk=id_cam)
     except (KeyError, ParkingCamera.DoesNotExist):
-        # Redisplay the question voting form.
-        return JsonResponse(variable1,safe=False)
+        return JsonResponse(zone_data, safe=False)
     else:
-        camera_of_spots.spots.append([x,y,width,height])
+        camera_of_spots.spots.append([x, y, width, height])
         camera_of_spots.save()
 
-    variable1['spots']=camera_of_spots.spots
-    return JsonResponse(variable1,safe=False)
-        # return JsonResponse({},safe=False)
-#
-   
+    zone_data['spots'] = camera_of_spots.spots
+    return JsonResponse(zone_data, safe=False)
+
 
 class BaseView(View):
     def global_context(self, request=None):
@@ -113,17 +84,30 @@ class BaseView(View):
     def render_json(self, json_dict: Dict[str, Any], **kwargs):
         return JsonResponse(json_dict, **kwargs)
 
+
+class CamDeleteView(DeleteView):
+    model = ParkingCamera
+    template_name = 'parking/cam_delete.html'
+    success_url = reverse_lazy('parking/cam_list.html')
+
+
+class CamListView(ListView):
+    model = ParkingCamera
+    template_name = 'parking/cam_list.html'
+
+
+class ManageSpots(UpdateView):
+    model = ParkingCamera
+    fields = ('spots', 'geopoint',)
+    template_name = 'parking/manage_spots.html'
+
+
 class CamCreateView(BaseView):
     template = 'parking/cam_add.html'
-    def context(self):
-        return {
-            'MAP_API': settings.MAP_API,
-            'FETCH_ZONES_INTERVAL': settings.FETCH_ZONES_INTERVAL,
-            'MAX_ZONE_REQUESTS': settings.MAX_ZONE_REQUESTS,
-        }
 
     def get(self, request):
-        return self.render_template(request, self.context())
+        return self.render_template(request, {'MAP_API': settings.MAP_API})
+
 
 class Parking(BaseView):
     template = 'parking/home.html'
@@ -143,6 +127,7 @@ class Zones(BaseView):
     def get(self, request):
         lat = Decimal(request.GET.get('latitude'))
         lng = Decimal(request.GET.get('longitude'))
+
         closest_cams = [
             cam for cam in ParkingCamera.objects.filter(is_active=True)
             if coords_in_radius(
@@ -155,7 +140,7 @@ class Zones(BaseView):
         assert len(closest_cams) < 2,\
             'Multiple cameras detection not implemented yet'
 
-        parking_data = []
+        parking_data = [] if closest_cams else {'error': 'No near cameras'}
 
         for cam in closest_cams:
             if not cam.spots:
@@ -173,33 +158,8 @@ class Zones(BaseView):
                 continue
 
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            cam_spots = {
-                'shortid': cam.short_id,
-                'coords': cam.coords,
-                'total_spots': len(cam.spots),
-                'free_spots': 0,
-                'frame': cam.last_frame,
-            }
-            retval, buffer = cv2.imencode('.jpg', frame)
-            drawframe=cv2.resize(
-                frame.copy(), (800, 400), interpolation=cv2.INTER_CUBIC
-            )
-            retval, buffer = cv2.imencode('.jpg', drawframe)
-            jpg_as_text = base64.b64encode(buffer)
-            txt=str(jpg_as_text)
-            
-            cam_spots['frame']=txt[2:-1]
-
-        parking_data.append(cam_spots)
-        return self.render_json(parking_data, safe=False)
-'''     for cam in closest_cams:
-            cap = cv2.VideoCapture(cam.url)
-            success, frame = cap.read()
-            if not success:
-                continue
-
             print(f"[+] Performing detection for cam '{cam.short_id}'")
-            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            detection_start = time.time()
             with graph.as_default():
                 results = model.detect([rgb_frame], verbose=0)
             print(f"[!] Detection took {time.time() - detection_start} secs")
@@ -238,5 +198,8 @@ class Zones(BaseView):
                 f"be greater than 'total_spots' ({cam_spots['total_spots']})"
 
             parking_data.append(cam_spots)
-            cv2.imwrite(f"{settings.MEDIA_ROOT}/{cam.short_id}.png", frame)
-'''
+            ratio = frame.shape[0] / frame.shape[1]
+            resized = cv2.resize(frame, (640, int(ratio * 640)))
+            cv2.imwrite(f"{settings.MEDIA_ROOT}/{cam.short_id}.png", resized)
+
+        return self.render_json(parking_data, safe=False)
